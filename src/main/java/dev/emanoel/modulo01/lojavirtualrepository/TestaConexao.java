485527f8da985a1d76a9454244ba9785e2a6b37e
emanoelcampos <emanoel.cv@gmail.com>
2023-08-04 15:20:09 -0300

    estabelecendo conexão com banco de dados

---
+++ src/main/java/dev/emanoel/modulo01/lojavirtualrepository/TestaConexao.java
@@ -0,0 +1,24 @@
+package dev.emanoel.modulo01.lojavirtualrepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+    public static void main(String[] args) {
+
+        String connectionSring = "jdbc:sqlserver://localhost:1433;databaseName=ContosoRetailDW;user=sqlserver;password=12345;trustServerCertificate=true";
+
+        try{
+            try (Connection connection = DriverManager.getConnection(connectionSring)) {
+                System.out.printf("Connection established.");
+            }
+        } catch (SQLException e) {
+            System.out.printf("Error connection to the database.");
+            e.printStackTrace();
+        }
+
+    }
+
+}
