c004846847fc30842ad015de91d2d46e77f1ee5b
emanoelcampos <emanoel.cv@gmail.com>
2023-08-04 16:29:44 -0300

    retorno de um statement com execute

--- src/main/java/dev/emanoel/modulo02/loja/statement/TesteListagem.java
+++ src/main/java/dev/emanoel/modulo02/loja/statement/TesteListagem.java
@@ -1,4 +1,4 @@
-package dev.emanoel.modulo02.loja.repository;
+package dev.emanoel.modulo02.loja.statement;
 
 import java.sql.*;
 
@@ -18,25 +18,9 @@ public class TesteListagem {
 
             // execute retorna um booleano
             // true = lista (select) / false = diferente de lista(delete, update, insert)
-            //boolean result = statement.execute("SELECT TOP (10) * FROM DimProduct");
-            //System.out.println(result);
+            boolean result = statement.execute("SELECT TOP (10) * FROM DimProduct");
+            System.out.println(result);
 
-            statement.execute("SELECT TOP(10) ProductKey, ProductName, BrandName FROM DimProduct");
-            ResultSet resultSet = statement.getResultSet();
-
-            while(resultSet.next()) {
-                Integer productKey = resultSet.getInt("ProductKey");
-                System.out.println(productKey);
-
-                String productName = resultSet.getString("ProductName");
-                System.out.println(productName);
-
-                String brandName = resultSet.getString("BrandName");
-                System.out.println(brandName);
-
-                System.out.println("--------------------------");
-
-            }
 
         } catch (SQLException e) {
             System.out.println("Error connection to the database.");
