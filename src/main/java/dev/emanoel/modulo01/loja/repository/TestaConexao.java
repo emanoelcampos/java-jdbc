1cb7ad87c6598109a4d0076e59dd75bbf135c108
emanoelcampos <emanoel.cv@gmail.com>
2023-08-04 15:36:31 -0300

    tratamento de exceção refatorado e otimização de packages

---
+++ src/main/java/dev/emanoel/modulo01/loja/repository/TestaConexao.java
@@ -0,0 +1,25 @@
+package dev.emanoel.modulo01.loja.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+    public static void main(String[] args) {
+
+        String connectionSring =
+                "jdbc:sqlserver://localhost:1433;" +
+                        "databaseName=ContosoRetailDW;" +
+                        "user=sqlserver;password=12345;" +
+                        "trustServerCertificate=true";
+
+        try (Connection connection = DriverManager.getConnection(connectionSring)) {
+            System.out.printf("Connection established.");
+        } catch (SQLException e) {
+            System.out.printf("Error connection to the database.");
+            e.printStackTrace();
+        }
+    }
+
+}
