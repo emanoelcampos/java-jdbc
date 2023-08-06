fc06f4a40aa9e16bab3f258ea4cafde66a83394b
emanoelcampos <emanoel.cv@gmail.com>
2023-08-05 23:30:03 -0300

    criando um método que recebe os parâmtros da inserção SQL e assumindo controle setando o auto commit como falso para garantir rollback em casos de erros durante a inserção de mais de uma linha na tabela

---
+++ src/main/java/dev/emanoel/modulo04/loja/commit/CreateProduct.java
@@ -0,0 +1,50 @@
+package dev.emanoel.modulo04.loja.commit;
+
+import dev.emanoel.modulo02.loja.connectionfactory.CriaConexao;
+
+import java.sql.*;
+
+public class CreateProduct {
+
+    public static void main(String[] args) throws SQLException {
+
+        CriaConexao criaConexao = new CriaConexao();
+        Connection connection = criaConexao.recuperaConexao();
+        connection.setAutoCommit(false);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO DimProduct(ProductName, ColorName) VALUES(?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            criaProduto("Teclado", "Logitech", preparedStatement);
+            criaProduto("Mouse", "Logitech", preparedStatement);
+
+            connection.commit();
+            preparedStatement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Rollback foi executado.");
+            connection.rollback();
+        }
+    }
+
+    private static void criaProduto(String nomeProduto, String corProduto, PreparedStatement preparedStatement) throws SQLException{
+
+        preparedStatement.setString(1, nomeProduto);
+        preparedStatement.setString(2, corProduto);
+
+        /*if(nomeProduto.equals("Mouse")) {
+            throw new RuntimeException("Não é possível adicionar o produto");
+        }*/
+
+        preparedStatement.execute();
+
+        ResultSet resultSetKey = preparedStatement.getGeneratedKeys();
+        while(resultSetKey.next()) {
+            Integer id = resultSetKey.getInt(1);
+            System.out.println(id);
+        }
+        resultSetKey.close();
+    }
+}
