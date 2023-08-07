c503b35bf579cf6e2d92e00e44c07d053ac962cb
emanoelcampos <emanoel.cv@gmail.com>
2023-08-07 13:38:34 -0300

    setando um número máximo de conexões

---
+++ src/main/java/dev/emanoel/modulo05/poolconnection/PoolSizeConnection.java
@@ -0,0 +1,28 @@
+package dev.emanoel.modulo05.poolconnection;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PoolSizeConnection {
+
+    public DataSource dataSource;
+
+    public PoolSizeConnection() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:sqlserver://localhost:1433;database=ContosoRetailDW;trustServerCertificate=true");
+        comboPooledDataSource.setUser("sqlserver");
+        comboPooledDataSource.setPassword("12345");
+
+        comboPooledDataSource.setMaxPoolSize(15);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection recuperaConexao() throws SQLException {
+
+        return this.dataSource.getConnection();
+    }
+}
