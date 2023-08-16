75bc20109bcd0f5137e02444a56d2881a8cbc743
emanoelcampos <emanoel.cv@gmail.com>
2023-08-15 23:48:09 -0300

    implemetação do método que lista categoria e produtos

--- src/main/java/dev/emanoel/modulo08/lojavirtual/dao/CategoryDAO.java
+++ src/main/java/dev/emanoel/modulo08/lojavirtual/dao/CategoryDAO.java
@@ -1,6 +1,7 @@
 package dev.emanoel.modulo08.lojavirtual.dao;
 
 import dev.emanoel.modulo08.lojavirtual.model.Category;
+import dev.emanoel.modulo08.lojavirtual.model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -31,6 +32,32 @@ public class CategoryDAO {
                             resultSet.getInt(1),
                             resultSet.getString(2));
                     categories.add(category);
+                }
+            }
+        }
+        return categories;
+    }
+
+    public List<Category> listWithProduct() throws SQLException {
+        Category last = null;
+        List<Category> categories = new ArrayList<>();
+
+        String selectQuery = "SELECT c.id, c.nome, p.id, p.nome, p.descricao FROM categoria c INNER JOIN produto p ON c.id = p.categoria_id";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+            preparedStatement.execute();
+
+            try(ResultSet resultSet = preparedStatement.getResultSet()) {
+                while(resultSet.next()) {
+                    if(last == null || !last.getNome().equals(resultSet.getString(2))) {
+                        Category category = new Category(
+                                resultSet.getInt(1),
+                                resultSet.getString(2));
+                        last = category;
+                        categories.add(category);
+                    }
+                    Product product = new Product(resultSet.getInt(3), resultSet.getString(4), resultSet.getString(5));
+                    last.addProduct(product);
                 }
             }
         }
