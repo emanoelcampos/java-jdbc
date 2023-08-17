588d35732dffb9dd49daab3ff9528c72030b4e16
emanoelcampos <emanoel.cv@gmail.com>
2023-08-16 23:29:51 -0300

    classe da aplicação do crud de produtos e categorias

---
+++ src/main/java/dev/emanoel/modulo08/lojavirtual/Application.java
@@ -0,0 +1,14 @@
+package dev.emanoel.modulo08.lojavirtual;
+
+import dev.emanoel.modulo08.lojavirtual.view.ProductCategoryFrame;
+
+import javax.swing.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ProductCategoryFrame productCategoryFrame = new ProductCategoryFrame();
+        productCategoryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
