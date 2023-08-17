1e0c65338067f3fa82b55d664b80422b1d3239f3
emanoelcampos <emanoel.cv@gmail.com>
2023-08-16 23:30:22 -0300

    implementação da classe controller de categoria

---
+++ src/main/java/dev/emanoel/modulo08/lojavirtual/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package dev.emanoel.modulo08.lojavirtual.controller;
+
+import dev.emanoel.modulo08.lojavirtual.dao.CategoryDAO;
+import dev.emanoel.modulo08.lojavirtual.factory.ConnectionFactory;
+import dev.emanoel.modulo08.lojavirtual.model.Category;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryController {
+
+    private CategoryDAO categoryDAO;
+
+    public CategoryController() {
+        Connection connection = new ConnectionFactory().recoveryConnection();
+        this.categoryDAO = new CategoryDAO(connection);
+    }
+
+    public List<Category> list() {
+        return this.categoryDAO.listCategory();
+    }
+}
